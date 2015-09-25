9274948b4fdf2c97a171eee3173f545597b074ad
P.R. Petit <paul.robert.petit@gmail.com>
2015-09-24 23:16:00 -0700

    FirstCommit
    Learn Python the Hard Way Examples

---
+++ Learn_Python_the_Hard_Way/ex1.py
@@ -0,0 +1,8 @@
+#This comment box is for the purposes of ex2, which is just ex1 with this comment
+print "Hello World!"
+print "Hello again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
